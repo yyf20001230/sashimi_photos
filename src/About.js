43318d8c84f191979851d35b31a14dc3c417e858
Frank Yang <yyf20001230@gmail.com>
2024-08-19 22:37:12 -0700

    build complete for photography

--- src/About.js
+++ src/About.js
@@ -21,6 +21,8 @@ const AboutPage = () => {
                 film projects such as Applause For A Cause and TEDx. I am committed to creating cinematic lighting and true-story 
                 shots that evokes emotion. To schedule a photo shoot or videography session with me, feel free to reach out 
                 to me in the contact section below.
+
+                <i> Pssst...</i> check out this <a href="http://frankyang.me" target="_blank" rel="noreferrer" style={{ color: "orangered", textDecoration: "underline"}}>site</a> for my boring side!
                 <div>
                   <img src={Applause} id="applause" loading="lazy" alt="Applause" />
                   <img src={Tedx} id="ted" loading="lazy" alt="TEDx" />
